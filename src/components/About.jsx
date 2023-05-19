1fcb069c2896efd63b734d9568196814340dbe04
mochamadgalih <mochamadglh@gmail.com>
2023-05-19 19:30:44 +0700

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white "
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut soluta
+          sequi maiores vitae? Aspernatur fuga quibusdam dolore harum eligendi
+          ex sit expedita, dolorem, fugit odio at! Facere cupiditate illum,
+          omnis, modi voluptate, delectus voluptatum reiciendis maxime aut
+          deleniti corrupti distinctio veritatis libero nulla placeat! Odit
+          veritatis aut veniam fugit aperiam, dolorem mollitia voluptatem harum
+          nihil. Doloribus animi quam eligendi cumque.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
+          deserunt! Consequatur quasi excepturi labore iusto obcaecati, corrupti
+          ipsam! Tempore ipsum, at ad quaerat ex aspernatur reprehenderit
+          perferendis excepturi magnam consequuntur sed omnis numquam iusto,
+          minima aperiam cumque veritatis. Qui eum vero, rerum repellat iusto
+          corporis atque aut distinctio. Unde placeat doloremque cumque tempore,
+          necessitatibus vel expedita. Soluta earum minima exercitationem, hic
+          libero necessitatibus est, totam debitis recusandae officia et ipsam?
+        </p>
+      </div>
+    </div>
+  );
+};
