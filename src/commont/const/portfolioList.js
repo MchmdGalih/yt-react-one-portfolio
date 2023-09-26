b7af15a00beab559079cbad95e5402dcad1bb769
mochamadgalih <mochamadglh@gmail.com>
2023-09-26 20:35:14 +0700

    masih mobile

--- src/commont/const/portfolioList.js
+++ src/commont/const/portfolioList.js
@@ -1,33 +1,18 @@
-import productCard from "../../assets/portfolio/profileCard.png";
-import profileCard from "../../assets/portfolio/profileCard.png";
-import portoGallery from "../../assets/portfolio/portoGallery.png";
-import museumCandy from "../../assets/portfolio/museumCandy.png";
-import suitJawa from "../../assets/portfolio/suitJawa.png";
-import linktreeMini from "../../assets/portfolio/linktreeMini.png";
+import todoApps from "../../assets/portfolio/todoapps.png";
+import webResponsif from "../../assets/portfolio/webResponsif.png";
+import museumOfCandy from "../../assets/portfolio/museumOfCandy.png";
 
 export const portfolios = [
   {
     id: 1,
-    src: productCard,
+    src: todoApps,
   },
   {
     id: 2,
-    src: museumCandy,
+    src: museumOfCandy,
   },
   {
     id: 3,
-    src: portoGallery,
-  },
-  {
-    id: 4,
-    src: suitJawa,
-  },
-  {
-    id: 5,
-    src: profileCard,
-  },
-  {
-    id: 6,
-    src: linktreeMini,
+    src: webResponsif,
   },
 ];
